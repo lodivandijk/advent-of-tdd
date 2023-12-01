8f6a6abe2dc645106e90443069660ab91b74ba42
pwouda-1973 <68119137+pwouda-1973@users.noreply.github.com>
2023-12-01 09:44:51 +0000

    Add more test cases for other lines

--- src/test/java/CalibrationDocLineShould.java
+++ src/test/java/CalibrationDocLineShould.java
@@ -15,4 +15,41 @@ public class CalibrationDocLineShould {
     var calibrationDocLine = new CalibrationDocLine("1abc2");
     assertThat(calibrationDocLine.getLastDigit(), equalTo(2));
  }
+
+ @Test
+ public void should_recognize_first_digit_line_2() {
+    var calibrationDocLine = new CalibrationDocLine("pqr3stu8vwx");
+    assertThat(calibrationDocLine.getFirstDigit(), equalTo(3));
+ }
+
+ @Test
+ public void should_recognize_last_digit_line2() {
+    var calibrationDocLine = new CalibrationDocLine("pqr3stu8vwx");
+    assertThat(calibrationDocLine.getLastDigit(), equalTo(8));
+ }
+
+  @Test
+ public void should_recognize_first_digit_line_3() {
+    var calibrationDocLine = new CalibrationDocLine("a1b2c3d4e5f");
+    assertThat(calibrationDocLine.getFirstDigit(), equalTo(1));
+ }
+
+ @Test
+ public void should_recognize_last_digit_line3() {
+    var calibrationDocLine = new CalibrationDocLine("a1b2c3d4e5f");
+    assertThat(calibrationDocLine.getLastDigit(), equalTo(5));
+ }
+
+   @Test
+ public void should_recognize_first_digit_line_4() {
+    var calibrationDocLine = new CalibrationDocLine("treb7uchet");
+    assertThat(calibrationDocLine.getFirstDigit(), equalTo(7));
+ }
+
+ @Test
+ public void should_recognize_last_digit_line4() {
+    var calibrationDocLine = new CalibrationDocLine("treb7uchet");
+    assertThat(calibrationDocLine.getLastDigit(), equalTo(7));
+ }
+
 }
